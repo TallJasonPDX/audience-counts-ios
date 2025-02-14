c27b91d814b72a0667ab540ff04c6e5f265dfa4d
talljasonpdx <37602681-talljasonpdx@users.noreply.replit.com>
2025-02-14 19:28:29 +0000

    User checkpoint: Update app logo and styling:  Replaced logo image, adjusted logo size and position for responsiveness, and updated header styling.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,15 +8,15 @@ import { ThemedView } from '@/components/ThemedView';
 export default function HomeScreen() {
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+      headerBackgroundColor={{ light: '#FFFFFF', dark: '#1D3D47' }}
       headerImage={
         <Image
-          source={require('@/assets/images/partial-react-logo.png')}
+          source={require('@/assets/images/as-tagline.jpg')}
           style={styles.reactLogo}
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome to Expo on Replit!</ThemedText>
+        <ThemedText type="title">Audience Synergy</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
@@ -58,17 +58,16 @@ const styles = StyleSheet.create({
   titleContainer: {
     flexDirection: 'row',
     alignItems: 'center',
-    gap: 8,
+    gap: 3,
   },
   stepContainer: {
     gap: 8,
     marginBottom: 8,
   },
   reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+    width: '80%',
+    zoom: 0.4,
+    textalign: 'center',
+    position: 'relative',
   },
 });
